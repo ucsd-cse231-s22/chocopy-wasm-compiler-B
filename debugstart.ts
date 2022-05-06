5600d9890d9c46c019e38d0ab8eef6e8085dfa02
Zhao Tang <tangzhao1999@hotmail.com>
2022-05-05 21:21:44 -0700

    reverted change on debugstart.ts

--- debugstart.ts
+++ debugstart.ts
@@ -6,10 +6,12 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-#String.wat
-class str:
-  def access(index:int):
-   ...`
+class C(object):
+  def f(self: C) -> int:
+    if True:
+      return 0
+    else:
+      return`
   const ast = parse(source);
   
   const repl = new BasicREPL(await addLibs());
