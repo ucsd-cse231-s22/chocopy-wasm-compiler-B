10ff7704d08e7722161b920fcc18565e217a7dfc
Zhao Tang <tangzhao1999@hotmail.com>
2022-05-05 21:16:56 -0700

    strings-design.md

--- debugstart.ts
+++ debugstart.ts
@@ -6,12 +6,10 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-class C(object):
-  def f(self: C) -> int:
-    if True:
-      return 0
-    else:
-      return`
+#String.wat
+class str:
+  def access(index:int):
+   ...`
   const ast = parse(source);
   
   const repl = new BasicREPL(await addLibs());
