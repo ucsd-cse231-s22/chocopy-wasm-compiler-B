969bf55dc812376f0103d4fd6f90f9c5114780d9
“MoonJiao123” <yuejiao459@gmail.com>
2022-05-11 21:30:51 -0700

    test case 10 11 12

--- tests/string.test.ts
+++ tests/string.test.ts
@@ -14,4 +14,16 @@ describe("string test", () => {
 print(s[0])`,
     ["a"]
   );
+
+  //10
+  assertTCFail("test case 10: should report type error ", `s:int = "asd"`);
+
+  //11
+  assertPrint("test case 11: use string as function parameters", `def f(s:str)->str:\n\treturn s\nprint(f("asd"))`, [`asd`]);
+
+  //12
+  assertPrint("test case 12: use string as class fields", `class C(object):\n\ts:str = "asd"\n\tdef gets(self: C)->str:\n\t\treturn self.s\nc:C = None\nc = C()\nprint(c.gets())`, [`asd`]);
+
+
+
 });
