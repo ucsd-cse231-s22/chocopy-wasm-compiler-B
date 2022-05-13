9be9794eadb7823014b5f21939f584ec635a91f3
Jeff Makings <jeffmakings@gmail.com>
2022-05-13 16:50:56 -0700

    binop import issues

--- runner.ts
+++ runner.ts
@@ -72,16 +72,19 @@ export async function run(source : string, config: Config) : Promise<[Value, Glo
   const [tprogram, tenv] = tc(config.typeEnv, parsed);
   const globalEnv = augmentEnv(config.env, tprogram);
   const irprogram = lowerProgram(tprogram, globalEnv);
-  const progTyp = tprogram.a[0];
+  let progTyp = tprogram.a[0];
   var returnType = "";
   var returnExpr = "";
   // const lastExpr = parsed.stmts[parsed.stmts.length - 1]
   // const lastExprTyp = lastExpr.a;
   // console.log("LASTEXPR", lastExpr);
-  if(progTyp !== NONE) {
+  if (progTyp === NUM) {
+    progTyp = NONE;
+    returnExpr = "(local.get $$last)\n(call $print_num)\n(local.set $$last)" // use set to consume the element on stack
+  } else if (progTyp === BOOL) {
     returnType = "(result i32)";
     returnExpr = "(local.get $$last)"
-  } 
+  }
   let globalsBefore = config.env.globals;
   // const compiled = compiler.compile(tprogram, config.env);
   const compiled = compile(irprogram, globalEnv);
@@ -105,6 +108,9 @@ export async function run(source : string, config: Config) : Promise<[Value, Glo
     (func $print_num (import "imports" "print_num") (param i32) (result i32))
     (func $print_bool (import "imports" "print_bool") (param i32) (result i32))
     (func $print_none (import "imports" "print_none") (param i32) (result i32))
+
+    (func $binop (import "imports" "binop") (param i32) (result i32))
+    
     (func $abs (import "imports" "abs") (param i32) (result i32))
     (func $min (import "imports" "min") (param i32) (param i32) (result i32))
     (func $max (import "imports" "max") (param i32) (param i32) (result i32))
