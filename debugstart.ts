038795d5024a7e4e53456c2c1a7ba28259a331af
Lesheng Jin <king18817550378@gmail.com>
2022-05-15 14:24:02 -0700

    debug

--- debugstart.ts
+++ debugstart.ts
@@ -8,21 +8,26 @@ async function debug() {
   var source = `
 class C(object):
   i: int = 0
-    
-c: C = None
-d: C = None
-c = C()
-test_refcount(c, 1)
-d = c
-test_refcount(c, 2)
-d = None
-test_refcount(c, 1)
-c = None`
+ 
+  def foo(self: C) -> C:
+    c0: C = None
+    c1: C = None
+    c0 = C()
+    c1 = C()
+    print(get_refcount(c0))
+    return c0
+
+v: C = None
+v = C().foo()
+print(get_refcount(C().foo()))
+print(get_refcount(v))
+`
   const ast = parse(source);
   
   const repl = new BasicREPL(await addLibs());
   const result = repl.run(source).then(result => {
-    console.log(result);    
+    // console.log(result);
+    console.log(importObject.output.trim().split("\n"));    
   })  
 }
 
