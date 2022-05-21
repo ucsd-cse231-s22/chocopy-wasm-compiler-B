e2a03a039fc53e78bd8b3e80df5da0eb740893ed
Ethan Nagola <enagola@ucsd.edu>
2022-05-21 10:57:33 -0700

    added more tests for expression based default values

--- tests/callingconventions.test.ts
+++ tests/callingconventions.test.ts
@@ -168,8 +168,102 @@ def test(x : int, y : int = 3, z : int = 5):
 test(1, 4)`,
     ["10"]
   );
-});
 
+  assertPrint("Default values using builtin1 functions work", `
+def test(builtIn : int = abs(-1)):
+  print(builtIn)
+  
+test()`, ["1"])
+
+  assertPrint("Default values using builtin2 functions work", `
+def test(builtIn : int = min(5,10)) -> int:
+  if(builtIn > 5):
+    return builtIn
+  else:
+    return 0
+    
+print(test())`, ["0"])
+
+assertPrint("Default values using methods from a class work", `
+class C(object):
+  def return5(self : C) -> int:
+    return 5
+
+  
+def test(fromC : int = C().return5()):
+  print(fromC)
+    
+test()`, ["5"])
+
+assertPrint("Default values using methods within a class work", `
+class C(object):
+  def return5(self : C) -> int:
+    return 5
+
+  def test(self : C, fromC : int = C().return5()):
+    print(fromC)
+    
+C().test()`, ["5"])
+
+assertPrint("Default values using both methods and fields within a class", `
+class C(object):
+  x : int = 5
+
+  def return5(self : C) -> int:
+    return 5
+
+  def test(self : C, fromC : int = C().x + C().return5()):
+    print(fromC)
+    
+C().test()`, ["10"])
+
+// assertPrint("Can use globals in default args", `
+// x : int = 5`)
+
+// de
+//   print(param)
+  
+// globalAsDef()
+// ""5[] ,:) x= tni  :marapf()DeAslaobgl
+assertPrint("can use the NOT uniop in default args", `
+b : bool = True
+
+def f(x : int, y : int, b2 : bool = not b):
+  if(b2):
+    print(x)
+  else:
+    print(y)
+
+f(4,8)`, ["8"])
+
+assertPrint("Use of NOT overrided by passed arg", `
+b : bool = True
+
+def f(x : int, y : int, b2 : bool = not b):
+  if(b2):
+    print(x)
+  else:
+    print(y)
+
+f(4,8,b)`, ["4"])
+
+assertPrint("Changing global variable does change default value", `
+b : bool = True
+
+def f(b2 : bool = b):
+  if(b2):
+    print(1)
+  else:
+    print(2)
+
+f()
+
+b = False
+
+f()`, ["1", "2"])
+
+
+});
 // Helpers
 
 /**
