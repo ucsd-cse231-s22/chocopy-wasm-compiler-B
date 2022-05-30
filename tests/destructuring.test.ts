c699742c6c4d7e6fd5fab18adb4bf0f59d705e90
1Divija1 <ddevarla@ucsd.edu>
2022-05-29 17:07:34 -0700

    sets and lists

--- tests/destructuring.test.ts
+++ tests/destructuring.test.ts
@@ -184,7 +184,7 @@ def range(s: int, e: int)->Range:
   return r
 `
 
-assertPrint("range-test", `
+assertPrint("range-test-1", `
 ${rangeDef}
 a:int = 5
 b:int = 3
@@ -194,7 +194,7 @@ print(b)
 `, ["1", "2"]
 )
 
-assertPrint("range-test", `
+assertPrint("range-test-2", `
 ${rangeDef}
 a:int = 5
 b:int = 3
@@ -203,6 +203,25 @@ print(a)
 print(b)
 `, ["2", "1"]
 )
+
+assertPrint("basic-destr-range-star-1", `
+${rangeDef}
+x : int = 0
+y : [int] = None
+x, *y = 2, range(1, 3)
+print(x)
+print(y[0])
+print(y[1])` , ["2", "1", "2"]);
+
+assertPrint("basic-destr-range-star-2", `
+${rangeDef}
+x : int = 0
+y : [int] = None
+x, *y = range(1, 4)
+print(x)
+print(y[0])
+print(y[1])` , ["1", "2", "3"]);
+
 
 assertPrint("destr-fnCallValidParam", `
 def f(a:int):
@@ -228,6 +247,12 @@ assertTCFail("length mismatch - lists", `
 x : int = 2
 y : int = 12
 x,y,z = [1,2]`);
+
+assertTCFail("type mismatch - list", `
+x : int = 2
+y : int = 12
+z : [bool] = True
+x,y,*z = 1,2,[True, False]`);
 
 assertTCFail("length mismatch - lists - binop", `
 x : int = 2
@@ -341,7 +366,7 @@ print(z[1])
 print(z[2])
 print(y)`, ["1","3","4","5","6"]);
 
-assertTCFail("Type Mismatch - lists - star", `
+assertTCFail("destr-list-Type Mismatch - lists - star", `
 x : int = 2
 y : set[int] = None
 x,*y = [2,3,6,7]`);
@@ -457,7 +482,7 @@ print(z[1][1])
 print(y[0])
 print(y[1])` , ["5", "6", "9", "10", "11", "12", "13", "14"]);
 
-assertPrint("basic-destr-index-assign", `
+assertPrint("basic-destr-index-assign-1", `
 x : int = 0
 y : int = 5
 a:[int] = None
@@ -468,6 +493,65 @@ x, a[0] = b[0], y
 print(x)
 print(a[0])
 print(a[1])` , ["3", "5", "3"]);
+
+assertPrint("basic-destr-index-assign-2", `
+x : int = 0
+y : int = 5
+a:[int] = None
+b:[int] = None
+a = [2,3]
+b = [3,4]
+x, a[0] = b[0], y
+print(x)
+print(a[0])
+print(a[1])` , ["3", "5", "3"]);
+
+// Have to support on lower side
+assertPrint("basic-destr-index-assign-3", `
+a : [int] = None
+y:int = 0
+x:int = 0
+a = [1,2,3]
+x, y, _ = a
+print(x)
+print(y)` , ["1", "2"]);
+
+assertTCFail("basic-destr-func-list-as-param-1", `
+def f(lst: [int]) -> int:
+  return lst[1]
+y : int = 0
+x : int = 0
+a : [int] = None
+a = [66, -5, 10]
+x, y, _ = f(a)
+print(x)
+print(y)`)
+
+assertPrint("basic-destr-func-list-as-param-2", `
+def f(lst: [int]) -> [int]:
+  return lst
+x : [int] = None
+y : int = 0
+a : [int] = None
+a = [66, -5, 10]
+x, y = f(a),6
+print(x[0])
+print(x[1])
+print(x[2])
+print(y)`, ["66", "-5", "10", "6"]);
+
+assertPrint("basic-destr-func-list-as-param-3", `
+def f(lst: [int]) -> [int]:
+  return lst
+x : int = 0
+y : int = 0
+z : int = 0
+a : [int] = None
+a = [66, -5, 10]
+x, y, z = f(a)
+print(x)
+print(y)
+print(z)`, ["66", "-5", "10"]);
 
 // Different types not supported inside List
 // assertPrint("destr-non paren-list", `
@@ -488,7 +572,7 @@ print(a[1])` , ["3", "5", "3"]);
 // x, _, *y = [[5, 6],[7, 8]]
 // print(x[0])
 // print(x[1])
-// print(y is None)` , ["5", "6", "True"]);
+// print(y)` , ["5", "6", "True"]);
 
 
 // Sets
@@ -502,6 +586,18 @@ assertTCFail("length mismatch - set", `
 x : int = 2
 y : int = 12
 x,y,z = {1,2}`);
+
+assertTCFail("length mismatch - set", `
+x : int = 2
+y : int = 12
+z = set[int] = None
+x,y,z = {1,{2,3}}`);
+
+assertTCFail("type mismatch - set", `
+x : int = 2
+y : int = 12
+c = set[bool] = None
+x,y,z = {1,{2,3}}`);
 
 assertTCFail("length mismatch - set - binop", `
 x : int = 2
@@ -537,8 +633,139 @@ assertPrint("basic-destr-multiple-set-ignore", `
 a : set[int] = None
 a,_ = {1,2},{3,4}
 print(1 in a)
-print(2 in a)` , ["True", "True"]);
+print(2 in a)
+print(3 in a)
+print(4 in a)` , ["True", "True", "False", "False"]);
 
+assertPrint("basic-destr-set-remove-1", `
+  set_1 : set[int] = None
+  set_1 = {1,2,5,6,7}
+  set_1.remove(5)
+  set_1,_ = set_1,set_1
+  print(1 in set_1)
+  print(2 in set_1)
+  print(5 in set_1)
+  print(6 in set_1)
+  print(7 in set_1)`, ["True", "True", "False", "True", "True"]);   
+
+// Need support in lower
+// assertPrint("basic-destr-set-remove-2", `
+//   set_1 : set[int] = None
+//   x : int = 0
+//   y : int = 0
+//   set_1 = {1,2,5,6,7}
+//   set_1.remove(5)
+//   set_1.remove(1)
+//   x,y,_ = set_1
+//   print(x)
+//   print(y)`, ["2", "6"]);  
+
+assertPrint("basic-destr-set", `
+x : int = 0
+y : int  = 0
+x, y = {5, 6}
+print(x)
+print(y)` , ["5", "6"]);
+
+assertPrint("destr-underscore-set", `
+x : int = 0
+y : int  = 0
+x,_, y = {5, 6, 7}
+print(x)
+print(y)` , ["5", "7"]);
+
+assertPrint("destr-fnCallValidParam-set", `
+def f(a:int):
+    x : int = 0
+    y : int = 0
+    z : int = 0
+
+    x, y, z = {a + 1 , a + 2, a + 3}
+    print(x)
+    print(y)
+    print(z)
+
+f(5)` , ["6", "7", "8"]);
+
+assertPrint("destr-fnCallValidParam-set-ignore-1", `
+def f(a:int):
+    x : int = 0
+    y : int = 0
+    z : int = 0
+
+    x, y,_,z= {a + 1 , a + 2, a + 3, a + 4}
+    print(x)
+    print(y)
+    print(z)
+
+f(5)` , ["6", "7", "9"]);
+
+
+assertPrint("destr-fnCallValidParam-set-ignore-2", `
+def f(a:int):
+    x : int = 0
+    y : int = 0
+    z : int = 0
+
+    _,x,y,z = {a + 1 , a + 2, a + 3, a + 4}
+    print(x)
+    print(y)
+    print(z)
+
+f(5)` , ["7", "8", "9"]);
+
+//Need support in lower
+// assertPrint("basic-destr-set-add-1", `
+// x_a : int = 0
+// y_a : int = 0
+// z_a : int = 0
+// x_b : int = 0
+// y_b : int = 0
+// z_b : int = 0
+// a : set[int] = None
+// b : set[int] = None
+// a = {2,3}
+// b = {3,4}
+// a.add(4)
+// b.add(5)
+// x_a,y_a,z_a = a
+// x_b,y_b,z_b = b
+// print(x_a)
+// print(y_a)
+// print(z_a)
+// print(x_b)
+// print(y_b)
+// print(z_b)` , ["2", "3", "4", "3", "4", "5"]);
+
+// Need support in lower
+// assertPrint("destr-fnCallValidParam-sets", `
+// def f(a:int)-> int:
+//     x : int = 0
+//     y : int = 0
+
+//     x, y = [a + 1 , a + 2]
+//     if a > 5:
+//       return x
+//     else:
+//       return y
+
+// a : set[int] = None
+// x_1 : int = 0
+// x_2 : int = 0
+// x_3 : int = 0
+// x_4 : int = 0
+// a.add(f(4))
+// a.add(f(5))
+// a.add(f(6))
+// a.add(f(7))
+// x_1,x_2,x_3,x_4 = a
+// print(x_1)
+// print(x_2)
+// print(x_3)
+// print(x_4)` , ["4", "5", "6", "7"]);
+
+
+// Set in a set case not supported yet
 // assertPrint("basic-destr-multiple-set-star-ignore", `
 // a : set[int] = None
 // c : set[int] = None
@@ -546,6 +773,28 @@ print(2 in a)` , ["True", "True"]);
 // print(a[0])
 // print(a[1])` , ["1", "2"]);
 
-});
 
-// [[1,2],[3,4]],sets
+
+// Class
+
+// assertPrint("basic-descr-class-field-assign",
+// `
+// class C(object):
+//   x : [int] = None
+//   def getX(self: C) -> int:
+//     return self.x
+//   def setX(self: C, x: int):
+//     self.x = x
+
+// c : C = None
+// c = C()
+// x_1 : int = 0
+// x_2 : int = 0
+// x_2 : int = 0
+// c.x = [1, 2, 3]
+// x_1, x_2, x_3 = c.x 
+// print(x_1)
+// print(x_2)
+// print(x_3)`,["1", "2", "3"]);
+
+});
