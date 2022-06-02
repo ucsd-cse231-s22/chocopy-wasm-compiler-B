9edf5d93336553e72729f94ce0d5438972a6a9d2
dai98 <dai1998555@hotmail.com>
2022-06-01 22:19:12 -0700

    Fix conflict

--- debugstart.ts
+++ debugstart.ts
@@ -5,33 +5,19 @@ import { importObject, addLibs  } from "./tests/import-object.test";
 // entry point for debugging
 async function debug() {
   var source = `
-  a: [int] = None
-  set_x: set[int] = None
-  set_x = set([1,2,3])
-  set_x.update([2,3,4])
-  print(set_x)
+a:bool = True
+b:bool = False
+b = a and False
+print(a)
+print(b)
   `
-
-  // set_1 : set[int] = None
-  // a : int = 0
-  // b : bool = True
-  // l: [int] = None
-  // set_1 = set([1, 2, 3])
-
-  // set_1.add(3)
-  // set_1.add(3)
-  // set_1.remove(1)
-  // a = len(set_1)
-  // b = 1 in set_1
-  // print(a)
-  // print(b)
   const ast = parse(source);
   console.log(`AST: \n ${ast}`)
   
   const repl = new BasicREPL(await addLibs());
   // const result = repl.tc(source);
   // console.log(result);
-  const result = repl.run(source).then(result => {
+  const result = repl.run(source, true, true).then(result => {
     console.log(result);
   })
 }
