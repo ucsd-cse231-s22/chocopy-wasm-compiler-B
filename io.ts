b883814e07b38dc29a46bebc1b3beb1d4fe5419e
arthur-x <arthur_x@126.com>
2022-06-01 17:39:47 -0700

    for loop works

--- io.ts
+++ io.ts
@@ -7,6 +7,39 @@ declare global {
 
 export const fileObjectDefinition : string = 
 `
+class __range__(object):
+    start: int = 0
+    stop: int = 0
+    step: int = 1
+    hasNext: bool = False
+    currval: int = 0
+    def __init__(self: __range__):
+        pass
+    def new(self: __range__, start: int, stop: int, step: int) -> __range__:
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.currval = start
+        return self
+
+    def next(self: __range__) -> int:
+        prev: int = 0
+        prev = self.currval
+        self.currval = prev+self.step
+        return prev
+        
+    def hasnext(self: __range__) -> bool:
+        nextval: int = 0
+        nextval = self.currval
+        if((self.step>0 and nextval<self.stop) or (self.step<0 and nextval>self.stop)):
+            self.hasNext = True
+        else:
+            self.hasNext = False
+        return self.hasNext
+
+def range(start: int, stop: int, step: int) -> __range__:
+    return __range__().new(start, stop, step)
+
 class File(object):
     fd : int = 0          
     pointer : int = 0
@@ -14,6 +47,12 @@ class File(object):
 
     def __init__(self : File):
         pass
+    
+    def next(self : File) -> int:
+        return self.read()
+    
+    def hasnext(self : File) -> bool:
+        return self.pointer < self.filelength
 
     def read(self : File) -> int:
         res : int = 0
